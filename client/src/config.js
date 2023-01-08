0938cc29a0d0c9dc2339578cfa6b9c1e35551f46
Ayushi Srivastava <89898383+a-tanvi@users.noreply.github.com>
2023-01-08 20:03:57 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : "https://hopeescope.herokuapp.com/api/"
+    baseURL : "https://hopescope.onrender.com/api"
 })
